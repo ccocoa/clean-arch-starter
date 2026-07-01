e5df091e23f0239fedd1294e9f9b9ba6321f2499
ccocoa <270871570+ccocoa@users.noreply.github.com>
2026-07-01 10:43:55 +0000

    chore: partial changes from agent
    Co-authored-by: thirapi <132630759+thirapi@users.noreply.github.com>

---
+++ src/interface-adapters/controllers/todo/TodoController.ts
@@ -0,0 +1,10 @@
+import { CreateTodoUseCase } from '../../../application/todo/use-cases/CreateTodoUseCase.js';
+import { TodoRepository } from '../../../domain/todo/repositories/TodoRepository';
+
+export class TodoController {
+  constructor(private createTodoUseCase: CreateTodoUseCase) {}
+
+  async createTodo(title: string) {
+    return await this.createTodoUseCase.execute(title);
+  }
+}
