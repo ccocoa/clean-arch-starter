cd18441946048df2556f46651dc2758b50a0e53b
ccocoa <insomeniac67@gmail.com>
2026-07-07 13:46:43 +0700

    fix: integrate TodoController into server action

--- app/actions/todo.ts
+++ app/actions/todo.ts
@@ -2,10 +2,12 @@
 
 import { InMemoryTodoRepository } from '../../src/infrastructure/todo/repositories/InMemoryTodoRepository.js';
 import { CreateTodoUseCase } from '../../src/application/todo/use-cases/CreateTodoUseCase.js';
+import { TodoController } from '../../src/interface-adapters/controllers/todo/TodoController.js';
 
 const todoRepository = new InMemoryTodoRepository();
 const createTodoUseCase = new CreateTodoUseCase(todoRepository);
+const todoController = new TodoController(createTodoUseCase);
 
 export async function createTodoAction(title: string) {
-  return await createTodoUseCase.execute(title);
+  return await todoController.createTodo(title);
 }
