ba63d18f9589060286536b6355bca0f13b50772d
ccocoa <insomeniac67@gmail.com>
2026-07-07 13:49:44 +0700

    feat: add getAllTodosAction to actions/todo.ts

--- app/actions/todo.ts
+++ app/actions/todo.ts
@@ -2,12 +2,16 @@
 
 import { InMemoryTodoRepository } from '../../src/infrastructure/todo/repositories/InMemoryTodoRepository.js';
 import { CreateTodoUseCase } from '../../src/application/todo/use-cases/CreateTodoUseCase.js';
-import { TodoController } from '../../src/interface-adapters/controllers/todo/TodoController.js';
+import { GetAllTodosUseCase } from '../../src/application/todo/use-cases/GetAllTodosUseCase.js';
 
 const todoRepository = new InMemoryTodoRepository();
 const createTodoUseCase = new CreateTodoUseCase(todoRepository);
-const todoController = new TodoController(createTodoUseCase);
+const getAllTodosUseCase = new GetAllTodosUseCase(todoRepository);
 
 export async function createTodoAction(title: string) {
-  return await todoController.createTodo(title);
+  return await createTodoUseCase.execute(title);
+}
+
+export async function getAllTodosAction() {
+  return await getAllTodosUseCase.execute();
 }
